49af3b23e764b894e42b4f8722d1757968e415ed
David MacMahon <davidm@astro.berkeley.edu>
2021-04-20 13:12:21 -0700

    Cleanup julia "runner" scripts

--- bin/srtcoordinator.jl
+++ bin/srtcoordinator.jl
@@ -3,58 +3,9 @@
 exec julia --color=yes --startup-file=no "${BASH_SOURCE[0]}" "$@"
 =#
 
-using Redis
-using JSON
-using RadioInterferometry
+import Pkg
+@assert Pkg.project().name == "SRT"
+Pkg.instantiate()
 
-function update_status(redis, json)
-  status = JSON.parse(json)
-
-  values = [
-            "SRC_NAME=$(status["source"])",
-            "RA=$(round(hms2h(status["ra"])*15, digits=6))",
-            "RA_STR=$(status["ra"])",
-            "DEC=$(round(dms2d(status["dec"]), digits=6))",
-            "DEC_STR=$(status["dec"])",
-            "FRONTEND=$(status["rcvr"])"
-           ]
-
-  @debug values
-
-  publish(redis, "srt:///set", join(values, "\n"))
-
-  # Complete hack for now...
-  lofreq = status["lofreq"]
-  obsfreq0 = lofreq + 187.5 / 64 * 31.5
-
-  publish(redis, "srt://blc00/0/set", "OBSFREQ=$(obsfreq0 + 1*187.5)")
-  publish(redis, "srt://blc01/0/set", "OBSFREQ=$(obsfreq0 + 2*187.5)")
-end
-
-function run_controller(redishost="redishost")
-  redis = RedisConnection(host=redishost)
-  sub = open_subscription(redis)
-  subscribe(sub, "srtstatus", m->update_status(redis, m))
-
-  # Allow CTRL-C to generate InterruptException (requires Julia >= v1.5.0)
-  Base.exit_on_sigint(false)
-
-  try
-    while true
-      sleep(1)
-    end
-  catch e
-    if isa(e, InterruptException)
-      @debug "got CTRL-C"
-    else
-      @error e
-    end
-  end
-
-  @info "coordinator shutting down"
-  #unsubscribe(sub, "srtstatus")
-  disconnect(sub)
-  disconnect(redis)
-end
-
-run_controller()
+import SRT
+SRT.Coordinator.main(ARGS)
